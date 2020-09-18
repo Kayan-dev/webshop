1a3039340d9ffb1fcf81b2c01d339178b2caf609
Kayan-dev <kaya-development@outlook.com>
2020-09-18 14:52:58 +0200

    adding through, foreign key to product & join table

--- models/product.js
+++ models/product.js
@@ -8,7 +8,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      product.belongsToMany(models.category);
+      product.belongsToMany(models.category, {
+        through: "joinorderproduct",
+        foreignKey: "joinorderproductId",
+      });
       product.hasMany(models.joinorderproduct);
     }
   }
